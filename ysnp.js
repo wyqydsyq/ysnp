38a4f1f746f90dda67dc13c6c70fe293bf6ac4b5
pyrokinetiq <pyrokinetiq@gmail.com>
2014-03-06 12:01:36 +1000

    Added useage to doc

--- ysnp.js
+++ ysnp.js
@@ -15,5 +15,41 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+var length = 12;
+var salt = '!!FoeTwennyBlayzeItFaygot';
 
+// check if salt or length were specified
+if(typeof process.argv[2] != 'undefined'){
+	length = process.argv[2];
+}
 
+if(typeof process.argv[3] != 'undefined'){
+	salt = process.argv[3];
+}
+
+var rl = require('readline'), crypto = require('crypto');
+
+rl = rl.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+console.log('\n',
+	'***************************************************************', '\n',
+	'* YSNP — You Shall Not Pass!                                  *', '\n',
+	'* ——————————————————————————————————————————————————————————— *', '\n',
+	'* Please input the service name and your master password to   *', '\n', 
+	'* generate/retrieve a password for that service:              *', '\n',
+	'***************************************************************'
+);
+
+rl.question('Service: ', function(service){
+	rl.question('Password: ', function(master){
+		var hash = crypto.createHash('sha256');
+		var password = hash.update(service + salt + master, 'utf8');
+		var password = hash.digest('hex').slice(0, length);
+		
+		console.log('Password for ' + service + ':', '\n\n', password, '\n');
+		rl.close();
+	});
+});
--- ysnp.sh
+++ ysnp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-node ./ysnp.js
+node ./ysnp.js $1 $2
